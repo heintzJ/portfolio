f7081d007b4c1adc8a9dcec12adf32aaebd2fa9e
heintzJ <96451646+heintzJ@users.noreply.github.com>
2024-05-04 13:46:23 -0400

    Syntax highlighting

--- docs/my-terminal.js
+++ docs/my-terminal.js
@@ -1,17 +1,27 @@
 // commands holds the commands that can be used
 const commands = {
     help() {
-        term.echo(`List of available commands: ${help}`);
+        terminal.echo(`List of available commands: ${help_list}`);
     },
     echo(...args) {
-        term.echo(args.join(' '));
+        terminal.echo(args.join(' '));
     },
     clear() {},
     refresh () {
         this.clear();
         ready()
+    },
+    ls() {
+        terminal.echo(directories);
     }
 };
+
+const terminal = $('body').terminal(commands, {
+    greetings: false,
+    checkArity: false,
+    exit: false,
+    completion: true
+});
 
 // formatter creates a formatted list for the help command
 const formatter = new Intl.ListFormat('en', {
@@ -20,26 +30,42 @@ const formatter = new Intl.ListFormat('en', {
 });
 // command_list is a list of all the keys in the commands variable
 const command_list = Object.keys(commands);
-const formatted_list = command_list.map(cmd => {
-    return `<white class="command">${cmd}</white>`;
-});
-const help = formatter.format(formatted_list);
+const help_list = formatter.format(command_list);
 
 // set the font and load it
 const font = 'Star Wars';
 figlet.defaults({ fontPath: 'https://unpkg.com/figlet/fonts/' });
 figlet.preloadFonts([font], ready);
 
-const term = $('body').terminal(commands, {
-    greetings: false,
-    checkArity: false
+// syntax highlighting
+const cmd_regex = new RegExp(`^\s*(${command_list.join('|')}) (.*)`);
+$.terminal.new_formatter(function(string) {
+    return string.replace(cmd_regex, function(_, command, args) {
+        return `[[;white;]${command}] [[;aqua;]${args}]`;
+    });
 });
+
+// set the directories to store information and links
+const directories = {
+    projects: [
+        '',
+        '[[;white;]Projects]',
+        [
+            ['Online Club Management Platform',
+             'https://github.com/andrearcaina/vivid',
+             'A full-stack web application for a fitness club that enables easy management of the club. It includes user authentication, a messaging platform for all members and administrators, and a membership management system for administration.'
+            ]
+        ].map(([name, url, description = '']) => {
+            return `* <a href="${url}">${name}</a> &mdash; <white>${description}</white>`;
+        })
+    ].flat()
+};
 
 // on page ready, show the greeting
 function ready() {
     // use the echo function on the text render to make sure the 
     // text remains responsive while resizing page
-    term.echo(() => {
+    terminal.echo(() => {
         const ascii = render("Jack Heintz");
         return `[[;green;]${ascii}]\nWelcome to my website\nType [[b;white;]ls] to get started or [[b;white;]help] to get a list of commands`;
     });
@@ -47,7 +73,7 @@ function ready() {
 
 // render the text using textSync
 function render(text) {
-    const cols = term.cols();
+    const cols = terminal.cols();
     return figlet.textSync(text, {
         font: font,
         width: cols,
